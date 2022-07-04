189c7a0ef5688068ea8ef8ef2f88704ff208378e
hippopotacos <hippopotacos@gmail.com>
2022-07-04 12:14:25 +0900

    [修正]favicon、apple-touch-iconのコード修正

--- nuxt-app/pages/index.vue
+++ nuxt-app/pages/index.vue
@@ -29,7 +29,9 @@
       { name: 'twitter:card', content: 'summary_large_image' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/assets/images/favicon.svg' },
+      { rel: 'icon', type: 'image/png', href: '/assets/images/favicon.png' },
+      { rel: 'icon', type: 'image/svg+xml', href: '/assets/images/favicon.svg' },
+      { rel: 'apple-touch-icon', href: '/assets/images/apple-touch-icon.png' },
       { rel: 'preconnect', href: 'https://fonts.gstatic.com' },
       { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@500;700&display=swap' }
     ],
